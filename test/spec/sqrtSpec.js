e86b9021eb4376249f2840166114896e44a409c5
Ariya Hidayat <ariya.hidayat@gmail.com>
2013-10-02 22:24:18 -0700

    Add a test to check for thrown exception.

--- test/spec/sqrtSpec.js
+++ test/spec/sqrtSpec.js
@@ -4,4 +4,8 @@ describe("sqrt", function() {
     expect(My.sqrt(4)).toEqual(2);
   });
 
+  it("should throw an exception if given a negative number", function() {
+    expect(function(){ My.sqrt(-1); }).toThrow(new Error("sqrt can't work on negative number"));
+  });
+
 });
